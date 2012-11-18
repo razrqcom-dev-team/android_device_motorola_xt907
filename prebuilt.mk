b8732b8f283f2571aacc1e684699b97bc9e5d2a1
Emerson Pinter <dev@pinter.com.br>
2012-11-18 14:16:53 -0200

    Copy libraries to obj
    Needed to build camera from source

--- prebuilt.mk
+++ prebuilt.mk
@@ -2,4 +2,8 @@
 LOCAL_PATH := vendor/motorola/xt926
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/proprietary/lib/libacdbloader.so:obj/lib/libacdbloader.so
+	$(LOCAL_PATH)/proprietary/lib/libacdbloader.so:obj/lib/libacdbloader.so \
+	$(LOCAL_PATH)/proprietary/lib/libmmjpeg.so:obj/lib/libmmjpeg.so \
+	$(LOCAL_PATH)/proprietary/lib/libmmstillomx.so:obj/lib/libmmstillomx.so \
+	$(LOCAL_PATH)/proprietary/lib/libimage-jpeg-enc-omx-comp.so:obj/lib/libimage-jpeg-enc-omx-comp.so \
+	$(LOCAL_PATH)/proprietary/lib/liboemcamera.so:obj/lib/liboemcamera.so
