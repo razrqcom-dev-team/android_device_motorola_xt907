05aab04efdbd98d2178830defde0dc601913f1d4
dhacker29 <davidhackerdvm@gmail.com>
2012-11-25 20:29:54 -0200

    Add telephony properties

--- device_xt926.mk
+++ device_xt926.mk
@@ -3,7 +3,20 @@ $(call inherit-product, device/motorola/vanquish-common/device.mk)
 LOCAL_PATH := device/motorola/xt926
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	persist.radio.eons.enabled= true
-
+	telephony.lteOnCdmaDevice=1 \
+	ro.config.svlte1x=true \
+	persist.radio.eons.enabled= true \
+	ro.cdma.nbpcd=1 \
+	ro.cdma.home.operator.isnan=1 \
+	ro.telephony.gsm-routes-us-smsc=1 \
+	ro.cdma.otaspnumschema=SELC,1,80,99 \
+	ro.cdma.data_retry_config=max_retries=infinite,0,0,10000,10000,100000,10000,10000,10000,10000,140000,540000,960000 \
+	ro.gsm.data_retry_config=default_randomization=2000,max_retries=infinite,1000,1000,80000,125000,485000,905000 \
+	ro.gsm.2nd_data_retry_config=max_retries=1,15000 \
+	persist.radio.mode_pref_nv10=1 \
+	persist.radio.vrte_logic=2 \
+	persist.radio.0x9e_not_callname=1 \
+	persist.radio.skip_data_check=1 \
+	persist.ril.max.crit.qmi.fails=4 \
 
 $(call inherit-product-if-exists, vendor/motorola/xt926/xt926-vendor.mk)
