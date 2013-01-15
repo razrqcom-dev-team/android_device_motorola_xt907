10a66561e646dee8ef6d660b3425bd404a6418a7
Emerson Pinter <dev@pinter.com.br>
2013-01-15 15:25:20 -0200

    Fix fingerprint
    Change-Id: I2504f92bc5a7a6148cb3b09c5bf90a0b830a6588

--- cm.mk
+++ cm.mk
@@ -12,4 +12,4 @@ PRODUCT_RELEASE_NAME := xt907
 
 $(call inherit-product, device/motorola/xt907/full_xt907.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_BRAND=motorola PRODUCT_NAME=xt907 BUILD_PRODUCT=xt907 BUILD_FINGERPRINT=motorola/xt907/vanquish:4.0.4/7.7.1Q-144_VQW_S3-34:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_BRAND=motorola PRODUCT_NAME=xt907 BUILD_PRODUCT=xt907 BUILD_FINGERPRINT=motorola/xt907/scorpion_mini:4.1.1/9.8.1Q_25/35:user/release-keys
