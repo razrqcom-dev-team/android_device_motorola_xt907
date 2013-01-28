2864dfc99cefd4c1f3ca06eacd8754015465462a
dhacker29 <davidhackerdvm@gmail.com>
2013-01-28 16:19:47 -0600

    Add CDMA/LTE props

--- device_xt907.mk
+++ device_xt907.mk
@@ -15,28 +15,33 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=240
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	telephony.lteOnCdmaDevice=1 \
-	ro.config.svlte1x=true \
-	persist.radio.eons.enabled= true \
-	ro.cdma.nbpcd=1 \
-	ro.cdma.home.operator.isnan=1 \
-	ro.telephony.gsm-routes-us-smsc=1 \
-	ro.cdma.otaspnumschema=SELC,1,80,99 \
-	ro.cdma.data_retry_config=max_retries=infinite,0,0,10000,10000,100000,10000,10000,10000,10000,140000,540000,960000 \
-	ro.gsm.data_retry_config=default_randomization=2000,max_retries=infinite,1000,1000,80000,125000,485000,905000 \
-	ro.gsm.2nd_data_retry_config=max_retries=1,15000 \
-	persist.radio.mode_pref_nv10=1 \
-	persist.radio.vrte_logic=2 \
-	persist.radio.0x9e_not_callname=1 \
-	persist.radio.skip_data_check=1 \
-	persist.ril.max.crit.qmi.fails=4 \
+    telephony.lteOnCdmaDevice=1 \
+    ro.config.svlte1x=true \
+    persist.radio.eons.enabled= true \
+    ro.cdma.nbpcd=1 \
+    ro.cdma.home.operator.isnan=1 \
+    ro.telephony.gsm-routes-us-smsc=1 \
+    ro.cdma.otaspnumschema=SELC,1,80,99 \
+    ro.cdma.data_retry_config=max_retries=infinite,0,0,10000,10000,100000,10000,10000,10000,10000,140000,540000,960000 \
+    ro.gsm.data_retry_config=default_randomization=2000,max_retries=infinite,1000,1000,80000,125000,485000,905000 \
+    ro.gsm.2nd_data_retry_config=max_retries=1,15000 \
+    persist.radio.mode_pref_nv10=1 \
+    persist.radio.vrte_logic=2 \
+    persist.radio.0x9e_not_callname=1 \
+    persist.radio.skip_data_check=1 \
+    persist.ril.max.crit.qmi.fails=4 \
+    ro.cdma.home.operator.numeric=310004 \
+    ro.cdma.home.operator.alpha=Verizon \
+    ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
+    ro.telephony.default_network=10 \
+    ro.mot.ignore_csim_appid=true
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/config/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/config/gps.conf:system/etc/gps.conf \
 
 # IDC
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/idc/atmxt-i2c.idc:system/usr/idc/atmxt-i2c.idc
+    $(LOCAL_PATH)/idc/atmxt-i2c.idc:system/usr/idc/atmxt-i2c.idc
 
 #safestrap support
 PRODUCT_COPY_FILES += \
