36b24ca2c7980aad4a429c63faa2681bd45a8410
dhacker29 <davidhackerdvm@gmail.com>
2013-05-25 21:19:50 -0500

    Update fingerprint to 4.1.2

--- cm.mk
+++ cm.mk
@@ -12,9 +12,13 @@ TARGET_SCREEN_HEIGHT := 960
 TARGET_SCREEN_WIDTH := 540
 
 # Release name
-PRODUCT_RELEASE_NAME := xt907
+PRODUCT_RELEASE_NAME := XT907
 PRODUCT_NAME := cm_xt907
 
 $(call inherit-product, device/motorola/xt907/full_xt907.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_BRAND=motorola PRODUCT_NAME=xt907 BUILD_PRODUCT=xt907 BUILD_FINGERPRINT=motorola/xt907/scorpion_mini:4.1.1/9.8.1Q_25/35:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_BRAND=motorola \
+    PRODUCT_NAME=XT907_verizon \
+    BUILD_PRODUCT=scorpion_mini \
+    BUILD_FINGERPRINT=motorola/XT907_verizon/scorpion_mini:4.1.2/9.8.1Q-66/28:user/release-keys \
